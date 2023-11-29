477b8b8f2c9e4725073be6d689fb79722622e9c1
Max <maximilian.gelbrecht@posteo.de>
2023-11-29 17:42:42 +0100

    restrict CI to 1.9, deploy docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,5 +3,5 @@ using Documenter, Exercise7
 makedocs(sitename="Exercise7.jl Documentation")
 
 deploydocs(
-    repo = "github.com/white-alistair/Exercise7.jl.git",
+    repo = "github.com/maximilian-gelbrecht/Exercise7.jl.git",
 )
